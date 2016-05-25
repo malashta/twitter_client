85fc2cfe126ab56c327175bb8dbf9a143c6b4afa
smalashta <Sergey.Malashta@noveogroup.com>
2016-05-25 22:33:44 +0600

    first init

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -8,6 +8,9 @@
   /** @ngInject */
   function runBlock($log) {
 
+    document.addEventListener("deviceready", function () {
+      $cordovaPlugin.someFunction().then(success, error);
+    }, false);
     $log.debug('runBlock end');
   }
 
