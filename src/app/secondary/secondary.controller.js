e8122eabc24f872d1e181e9f10f74131f0a900f6
Tallerr <malashta@gmail.com>
2016-06-02 00:41:07 +0600

    add html pattern MD and SX for second

--- src/app/secondary/secondary.controller.js
+++ src/app/secondary/secondary.controller.js
@@ -11,12 +11,49 @@
   function SecondaryController($scope, twitterService, $localStorage, $log, baseActions){
     var vm = this;
     $scope.tweet = {};
+    var imagePath = '';
 
     twitterService.initialize();
     vm.dataBase = baseActions.initBase();
     vm.dataBase.$bindTo($scope, "tweet");
 
+    $scope.$watch('tweet',function(){
+      $log.log($scope.tweet);
+      vm.imagePath = $scope.tweet.user.profile_image_url;
+    });
+
     $localStorage.second = {status:'open'};
 
+    vm.messages = [{
+      face : vm.imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : vm.imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : vm.imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : vm.imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }, {
+      face : vm.imagePath,
+      what: 'Brunch this weekend?',
+      who: 'Min Li Chan',
+      when: '3:08PM',
+      notes: " I'll be in your neighborhood doing errands"
+    }];
   }
 })();
