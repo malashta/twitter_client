1b2a1d64fa34ddf381381d58fbe9b3708c534edf
smalashta <Sergey.Malashta@noveogroup.com>
2016-06-03 14:18:44 +0600

    correct second

--- src/app/secondary/secondary.controller.js
+++ src/app/secondary/secondary.controller.js
@@ -8,26 +8,33 @@
     .module('pl')
     .controller('SecondaryController', SecondaryController);
 
-  function SecondaryController($scope, twitterService, $localStorage, $log, baseActions){
+  function SecondaryController($scope, twitterService, $localStorage, $log, baseActions, $window){
     var vm = this;
+    vm.Date = $window.Date;
     $scope.tweet = {};
     vm.tweets = [];
+    vm.replyToTweet = replyToTweet;
     var imagePath = '';
 
     twitterService.initialize();
     vm.dataBase = baseActions.initBase();
     vm.dataBase.$bindTo($scope, "tweet");
 
-    $scope.$watch('tweet',function(){
+    $scope.$watch('tweet',function() {
       if($scope.tweet.hasOwnProperty('id')) {
         $log.log($scope.tweet);
         vm.tweets.push($scope.tweet);
         vm.imagePath = $scope.tweet.user.profile_image_url;
         twitterService.getUserTimeline($scope.tweet.user.screen_name).then(function(data){
           vm.tweets = data;
+          $log.log(data);
         });
       }
     });
+
+    function replyToTweet(tweet) {
+      $log.log(tweet);
+    }
 
     $localStorage.second = {status:'open'};
 
